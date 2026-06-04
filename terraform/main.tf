ae36804c9686306e8ce001d1ea3e228aaaf59a1d
Given Ndou <ndougiven3@gmail.com>
2026-06-04 03:24:27 +0000

    updated terraform version

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "= 5.80.0"
     }
   }
   # For the assignment, local state is used to keep things simple, 
