2550a13616dee7534fec367cbcb4aa9b4e858449
Given Ndou <ndougiven3@gmail.com>
2026-06-04 04:38:00 +0000

    addedoverwrite to true

--- terraform/main.tf
+++ terraform/main.tf
@@ -146,15 +146,13 @@ resource "aws_lb" "external" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb.id]
   subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
-  overwrite          = true
 }
 
 resource "aws_lb_target_group" "web" {
-  name      = "rewards-dev-tg"
-  port      = 80
-  protocol  = "HTTP"
-  overwrite = true
-  vpc_id    = aws_vpc.main.id
+  name     = "rewards-dev-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
 
   health_check {
     path                = "/health"
