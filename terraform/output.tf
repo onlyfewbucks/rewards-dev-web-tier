bb30c6324be7189019a9ab4ae9657bd5a9c0f3ef
onlyfewbucks <onlyfewbucks@gmail.com>
2026-06-05 09:06:54 +0200

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -3,7 +3,3 @@ output "alb_dns_name" {
   description = "Public entry point for the application"
 }
 
-output "instance_public_ip" {
-  value       = aws_instance.web.public_ip
-  description = "Used by the Ansible inventory string in CI/CD"
-}
